047d346ed6e7817222eb756bbda4f9cab1799b48
FrankRemmy <princeugo.remix@gmail.com>
2020-09-24 06:50:31 +0100

    Header, footer and add

---
+++ add.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include('templates/header.php'); ?>
+    
+    <section class="containter grey=text">
+    <h4 class="center">Add a Pizza</h4>
+    <form action="" class="white" method="" >
+        <label>Your Email:</label>
+        <input type="text" name="email">
+        <label>Pizza Title:</label>
+        <input type="text" name="title">
+        <label>Ingredients (comma seperated):</label>
+        <input type="text" name="ingredients">
+        <div class="center">
+            <input type="submit" name="submit" class="btn brand x-depth-0">
+        </div>
+    </form>
+    </section>
+
+    <?php include('templates/footer.php'); ?>
+    
+
+</html>
