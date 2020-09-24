ea7d3d40502a7e184d0ecadbbb2b18d56b8b0618
FrankRemmy <princeugo.remix@gmail.com>
2020-09-24 08:01:17 +0100

    Basic form validation

--- add.php
+++ add.php
@@ -1,3 +1,37 @@
+<?php 
+
+    // if(isset($_GET['submit'])) {
+    //     echo $_GET['email'];
+    //     echo $_GET['title'];
+    //     echo $_GET['ingredients'];
+    // }
+
+    if(isset($_POST['submit'])) {
+        if (empty($_POST['email'])) { // check email
+            echo 'An email is required. <br>';
+        } else {
+            echo htmlspecialchars($_POST['email']);
+        }
+
+        if (empty($_POST['title'])) { // check title
+            echo 'A title is required. <br>';
+        } else {
+            echo htmlspecialchars($_POST['title']);
+        }
+
+        if (empty($_POST['ingredients'])) { // check ingredients
+            echo 'At least one ingredient is required. <br>'; 
+        } else {
+            echo htmlspecialchars($_POST['ingredients']);
+        }
+
+        // echo htmlspecialchars($_POST['email']);
+        // echo htmlspecialchars($_POST['title']);
+        // echo htmlspecialchars($_POST['ingredients']);
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +39,7 @@
     
     <section class="containter grey=text">
     <h4 class="center">Add a Pizza</h4>
-    <form action="" class="white" method="" >
+    <form action="add.php" class="white" method="POST" >
         <label>Your Email:</label>
         <input type="text" name="email">
         <label>Pizza Title:</label>
@@ -13,7 +47,7 @@
         <label>Ingredients (comma seperated):</label>
         <input type="text" name="ingredients">
         <div class="center">
-            <input type="submit" name="submit" class="btn brand x-depth-0">
+            <input type="submit" name="submit" value="submit" class="btn brand x-depth-0">
         </div>
     </form>
     </section>
